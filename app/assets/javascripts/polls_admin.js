0580381ba4d46a9c7dbb842d96b6fa2817816d56
Javi Martín <javim@elretirao.net>
2019-07-02 16:42:28 +0200

    Add and apply ESLint spacing rules
    For now we're only adding rules related to spacing and double quotes,
    following the same rules we use in Ruby, which are the same rules
    CoffeeScript followed when compiling these files.
    
    We're also using the recommended ESLint rules, which will warn us about
    many JavaScript common pitfalls, the `strict` rule which enforces using
    strict mode, and the `no-console` rule, which will prevent us from
    shipping code meant for debugging.
    
    Although it's arguably more common to use the JSON format to define
    these rules, I've chosen YAML because it's the format we use in all our
    linters.

--- app/assets/javascripts/polls_admin.js
+++ app/assets/javascripts/polls_admin.js
@@ -5,13 +5,13 @@
       $("select[class='js-poll-shifts']").on({
         change: function() {
           switch ($(this).val()) {
-            case "vote_collection":
-              $("select[class='js-shift-vote-collection-dates']").show();
-              $("select[class='js-shift-recount-scrutiny-dates']").hide();
-              break;
-            case "recount_scrutiny":
-              $("select[class='js-shift-recount-scrutiny-dates']").show();
-              $("select[class='js-shift-vote-collection-dates']").hide();
+          case "vote_collection":
+            $("select[class='js-shift-vote-collection-dates']").show();
+            $("select[class='js-shift-recount-scrutiny-dates']").hide();
+            break;
+          case "recount_scrutiny":
+            $("select[class='js-shift-recount-scrutiny-dates']").show();
+            $("select[class='js-shift-vote-collection-dates']").hide();
           }
         }
       });
